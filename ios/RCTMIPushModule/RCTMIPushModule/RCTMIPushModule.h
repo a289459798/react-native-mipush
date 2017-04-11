75dfdce5b10075a8e4dbf015540ce2df370ea8b3
zhangzy <zhangzy@zhangzydeMacBook-Pro.local>
2017-04-11 18:58:58 +0800

    修改bug

--- ios/RCTMIPushModule/RCTMIPushModule/RCTMIPushModule.h
+++ ios/RCTMIPushModule/RCTMIPushModule/RCTMIPushModule.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UserNotifications.h>
-#import "RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
 #import "RCTPushNotificationManager.h"
 #import "MiPushSDK.h"
 
